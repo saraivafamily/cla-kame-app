3ab0f35078f78fb4823a2f8729e645d5ba5d04d3
saraivafamily <saraivafamily33@gmail.com>
2026-06-04 12:45:56 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged }
 import { getFirestore, doc, setDoc, updateDoc, onSnapshot, collection, getDocs } from 'firebase/firestore';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
