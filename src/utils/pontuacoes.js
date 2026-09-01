fb6ff92e0a6a1d96434ca7317e1f9726da05b0d5
saraivafamily <saraivafamily33@gmail.com>
2026-09-01 13:49:05 -0300

    Add scoring constants for NORMAL and FLASH categories

--- src/utils/pontuacoes.js
+++ src/utils/pontuacoes.js
@@ -1,1 +1,30 @@
+// src/utils/pontuacoes.js
 
+export const PONTOS = {
+  NORMAL: {
+    JOIN: 10,
+    PLAY: 2,
+    WIN: 3,
+    DRAW: 1,
+    OITAVAS: 5,
+    QUARTAS: 10,
+    SEMI: 15,
+    TERCEIRO: 15,
+    VICE: 25,
+    CAMPEAO: 50,
+    PUNICAO_WO: -10
+  },
+  FLASH: {
+    JOIN: 2,
+    PLAY: 1,
+    WIN: 1,
+    DRAW: 0,
+    OITAVAS: 0,
+    QUARTAS: 2,
+    SEMI: 5,
+    TERCEIRO: 5,
+    VICE: 10,
+    CAMPEAO: 20,
+    PUNICAO_WO: -10
+  }
+};
